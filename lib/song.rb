49b4d5eac372b8c060431cacb9b1b8f4f086003a
davidtom <davidjtomczyk@gmail.com>
2017-06-11 16:12:26 -0400

    update Song.new_by_filename code to be more succint, using Object#instance_eval

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist
   attr_reader
@@ -9,11 +11,16 @@ class Song
 
   def self.new_by_filename(file_name)
     #Expected filename format: 'Michael Jackson - Black or White - pop.mp3'
-    data_array = file_name.split(/[-]/)
-    fn_hash = {
-      artist: data_array[0].strip,
-      song: data_array[1].strip
-    }
+    # data_array = file_name.split(/[-]/) ## There has to be a more elegant way to do this. Maybe with yield or a lambda?
+    # fn_hash = {
+    #   artist: data_array[0].strip,
+    #   song: data_array[1].strip
+    # }
+    fn_hash = file_name.split(/[-]/).instance_eval do |data|
+      {artist: data[0].strip,
+        song: data[1].strip}
+    end
+    # binding.pry
     Song.new(fn_hash[:song]).tap do |s|
       s.artist = Artist.find_or_create_by_name(fn_hash[:artist])
       s.artist.add_song(s)
